2058137affe8f565b688e6e7288e809e88b3db3e
Jales Cardoso <jalescardoso@hotmail.com>
2023-02-05 09:32:33 -0300

    .

---
+++ frontend/angular.util.js
@@ -0,0 +1,23 @@
+angular.module('myService', [])
+    .service('MS', function ($http) {
+        this.actionHandler = async (func, options = {}) => {
+            if (!options.selectorLoad) options.selectorLoad = 'body';
+            try {
+                $(options.selectorLoad).loading({ message: 'Aguarde ...' });
+                let data = await func;
+                await new Promise(resolve => {
+                    $(options.selectorLoad).loading('stop');
+                    this.notifSucess(options.msgNotf);
+                    setTimeout(resolve, 500)
+                });
+                return data;
+            } catch (e) {
+                this.notifError(options.msgError);
+                throw e;
+            } finally {
+                $(options.selectorLoad).loading('stop');
+            }
+        }
+        this.notifSucess = (msg = null) => $.notify((msg || 'Ação realizada com sucesso'), "success");
+        this.notifError = (msg = null) => $.notify((msg || 'Ocorreu um erro inesperado'), "error");
+    })
