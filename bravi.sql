c02530fe69938566a83ab5e96a3b940dcf5e7cec
Jales Cardoso <jlscrds@gmail.com>
2023-02-06 10:45:11 -0300

    arquivo mysql criação do banco

---
+++ bravi.sql
@@ -0,0 +1,31 @@
+-- Adminer 4.8.1 MySQL 8.0.32-0ubuntu0.20.04.2 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `contato`;
+CREATE TABLE `contato` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id_pessoa` int unsigned NOT NULL,
+  `tipo` enum('Telefone','Whatsapp','Email') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `descricao` varchar(500) NOT NULL,
+  `valor` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_pessoa` (`id_pessoa`),
+  CONSTRAINT `contato_ibfk_1` FOREIGN KEY (`id_pessoa`) REFERENCES `pessoa` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+DROP TABLE IF EXISTS `pessoa`;
+CREATE TABLE `pessoa` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- 2023-02-06 13:44:48
