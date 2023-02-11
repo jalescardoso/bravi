fcc0de3a3d8cf0f22e541b36fa06dc53e9f17ac5
Jales Cardoso <jalescardoso@hotmail.com>
2023-02-11 19:04:29 -0300

    .

--- modelos/contato.php
+++ modelos/contato.php
@@ -5,33 +5,25 @@ namespace modelos;
 use Connector;
 use interfaces\{Model};
 
-class Pessoa implements Model {
+class Contato implements Model {
     public ?int $id;
-    public int $id_pessoa;
-    public string $descricao;
-    public string $tipo;
-    public float $valor;
+    public string $nome;
     public function __construct(
         protected Connector $mysql,
         array $model = null
     ) {
         if ($model) {
             $this->id   = $model['id'] ?: null;
-            $this->id_pessoa = $model['id_pessoa'] ?: throw new \Exception("Pessoa relacionada ao contato obrigatório");
-            $this->descricao = $model['descricao'] ?: throw new \Exception("Descrição obrigatório");
-            $this->tipo = $model['tipo'] ?: throw new \Exception("Tipo obrigatório");
-            $this->valor = $model['valor'] ?: throw new \Exception("Valor obrigatório");
+            $this->nome = $model['nome'] ?: throw new \Exception("Nome da pessoa obrigatório");
         }
     }
     public function getTableName(): string {
-        return "contato";
+        return "pessoa";
     }
     public function getObject(): array {
         return [
             "id"   => $this->id,
-            "id_pessoa" => $this->id_pessoa,
-            "descricao" => $this->descricao,
-            "valor" => $this->valor,
+            "nome" => $this->nome
         ];
     }
     public function save(): int {
